5c622b7cdca3a35bd2913aa672c7be52bb6e6da7
Adrian Ramirez <adriram99@gmail.com>
2025-09-08 07:51:56 -0300

    feat(products): add REST API endpoints

---
+++ products/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Product, Label
+
+class LabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ['id', 'name']
+
+class ProductSerializer(serializers.ModelSerializer):
+    labels = LabelSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'sku', 'price', 'stock', 'labels']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        user = self.context['request'].user
+        if user.is_authenticated and user.groups.filter(name='Viewer').exists():
+            # For viewers, don't show stock info
+            representation.pop('stock', None)
+        return representation
+
+class ProductCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'sku', 'price', 'stock']
--- products/views.py
+++ products/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Label
+from .serializers import ProductSerializer, ProductCreateUpdateSerializer, LabelSerializer
 
 # Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    # serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'sku', 'labels__name']
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return ProductCreateUpdateSerializer
+        return ProductSerializer
+
+    # POST /products/{id}/labels/ -> add label to product
+    @action(detail=True, methods=['post'])
+    def labels(self, request, pk=None):
+        product = self.get_object()
+        label_id = request.data.get('label_id')
+        label_name = request.data.get('label_name')
+        
+        if label_id:
+            try:
+                label = label.objects.get(id=label_id)
+            except Label.DoesNotExist:
+                return Response({'error': 'Label not found'}, status=status.HTTP_404_NOT_FOUND)
+        elif label_name:
+            label, _ = Label.objects.get_or_create(name=label_name)
+        else:
+            return Response({'error': 'Provide label_id or label_name'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        product.labels.add(label)
+        return Response(ProductSerializer(product).data, status=status.HTTP_200_OK)
+
+    # DELETE /products/{id}/remove_label/ -> remove label from product
+    @action(detail=True, methods=["delete"], url_path="labels/(?P<label_id>[^/.]+)")
+    def remove_label(self, request, pk=None, label_id=None):
+        product = self.get_object()
+        try:
+            label = Label.objects.get(id=label_id)
+        except Label.DoesNotExist:
+            return Response({"error": "Label not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        product.labels.remove(label)
+        return Response(ProductSerializer(product).data, status=status.HTTP_200_OK)
+    
+class LabelViewSet(viewsets.ModelViewSet):
+    queryset = Label.objects.all()
+    serializer_class = LabelSerializer
