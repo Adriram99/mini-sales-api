a32cea51acb42dd6bf323a160aa0981310424e5f
Adrian Ramirez <adriram99@gmail.com>
2025-09-08 07:49:14 -0300

    feat: delete phone number field in customer

--- customers/admin.py
+++ customers/admin.py
@@ -4,6 +4,6 @@ from .models import Customer
 # Register your models here.
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone_number')
-    search_fields = ('first_name', 'last_name', 'email', 'phone_number')
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
     ordering = ('last_name', 'first_name')
--- customers/models.py
+++ customers/models.py
@@ -5,7 +5,6 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, unique=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
