a8c87dd948ab8e9fa0531d70bfe0689be6dd3057
Adrian Ramirez <adriram99@gmail.com>
2025-09-09 09:06:06 -0300

    feat(products): add validations

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from products.models import Product
 from customers.models import Customer
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
 class Order(models.Model):
@@ -22,8 +24,8 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.DecimalField(max_digits=10, decimal_places=2)
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, validators=[MinValueValidator(0.00)])
 
     def subtotal(self):
         return self.unit_price * self.quantity
